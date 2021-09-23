dfa6cfe26f54996ac802a242d1be9427ab456add
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2021-09-23 14:00:49 -0400

    Update tweetdf_demo.py

--- DataProcessing/tweetdf_demo.py
+++ DataProcessing/tweetdf_demo.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 13 18:58:49 2021
-
-@author: 19522
+Simple Demonstration of using Twint to gather tweets from a particular account
+and store the tweets in a Pandas dataframe.
 """
 
 import csv
@@ -10,15 +9,21 @@ import twint
 import nest_asyncio
 import pandas as pd
 
-    
+# Required to use Twint    
 nest_asyncio.apply()
+# Create Twint object for a web-scrape query
 c = twint.Config() 
+# Designate the search to take place from the @nbcnews twitter account
 c.Search = "from:@nbcnews" 
+# We are storing the results in an object
 c.Store_object = True 
+# Limit the search to 20 results (most recent in this case)
 c.Limit=20
+# Necessary to incorporate pandas dataframe functionality
 c.Pandas=True
 #c.Store_csv = True
 #c.Output = 'nbc.csv'
+# Run the search on our 'c' twint object
 data = twint.run.Search(c) 
+# Store the search results in a 'Tweets_df' pandas df
 Tweets_df = twint.storage.panda.Tweets_df;
-    
