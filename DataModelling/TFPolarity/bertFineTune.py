85032c3891014af189f9f8eb733b07e25e90bb0c
plehman2000 <patricklehman2000@gmail.com>
2021-10-21 13:55:21 -0400

    First Polarity Model
    Added model, driver coder and wrapper class

---
+++ DataModelling/TFPolarity/bertFineTune.py
@@ -0,0 +1,106 @@
+# Libraries
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+
+# Preliminaries
+
+from torchtext.legacy.data import Field, TabularDataset, BucketIterator, Iterator
+
+# Models
+
+import torch.nn as nn
+from transformers import BertTokenizer, BertForSequenceClassification
+
+# Training
+
+import torch.optim as optim
+
+# Evaluation
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+
+
+
+class BERT(nn.Module):
+
+    def __init__(self):
+        super(BERT, self).__init__()
+
+        options_name = "bert-base-uncased"
+        self.encoder = BertForSequenceClassification.from_pretrained(options_name)
+
+    def forward(self, text, label):
+        loss, text_fea = self.encoder(text, labels=label)[:2]
+
+        return loss, text_fea
+    
+    
+    
+    
+    # Save and Load Functions
+
+def save_checkpoint(save_path, model, valid_loss):
+
+    if save_path == None:
+        return
+    
+    state_dict = {'model_state_dict': model.state_dict(),
+                  'valid_loss': valid_loss}
+    
+    torch.save(state_dict, save_path)
+    print(f'Model saved to ==> {save_path}')
+
+def load_checkpoint(load_path, model):
+    
+    if load_path==None:
+        return
+    
+    state_dict = torch.load(load_path, map_location=device)
+    print(f'Model loaded from <== {load_path}')
+    
+    model.load_state_dict(state_dict['model_state_dict'])
+    return state_dict['valid_loss']
+
+
+def save_metrics(save_path, train_loss_list, valid_loss_list, global_steps_list):
+
+    if save_path == None:
+        return
+    
+    state_dict = {'train_loss_list': train_loss_list,
+                  'valid_loss_list': valid_loss_list,
+                  'global_steps_list': global_steps_list}
+    
+    torch.save(state_dict, save_path)
+    print(f'Model saved to ==> {save_path}')
+
+
+def load_metrics(load_path):
+
+    if load_path==None:
+        return
+    
+    state_dict = torch.load(load_path, map_location=device)
+    print(f'Model loaded from <== {load_path}')
+    
+    return state_dict['train_loss_list'], state_dict['valid_loss_list'], state_dict['global_steps_list']
+
+
+
+
+import os
+
+
+def main():
+    
+    source_folder = os.getcwd() + '/tut_data/'
+    destination_folder = os.getcwd() + '/tut_data/dest'
+    
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    
+    
+    
