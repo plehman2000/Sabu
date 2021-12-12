cede1dde32f46221882c0601ef2d9f9c5018522a
plehman2000 <patricklehman2000@gmail.com>
2021-12-12 16:00:34 -0500

    Added getToxicity Model and Function, along with testing notebook
    In order to use the toxicity model, you gotta
    1) download the checkpoint
    
    https://drive.google.com/file/d/1qcVZQ1PVzqGxhxdtIM9WLLy1fb-NGBaX/view?usp=sharing
    
    2) extract it to the getTox directory, so it's in a separate folder from getToxicity.py

---
+++ DataModelling/getTox/getToxicity.py
@@ -0,0 +1,47 @@
+#USING TENSORFLOW 2.4.1
+
+from transformers import TFBertModel
+from utils import bert_utils, plots
+import os
+import numpy as np
+
+np.set_printoptions(precision=2)
+import tensorflow as tf
+
+from tensorflow import keras
+import tensorflow.keras.layers as layers
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+
+N_CLASSES = 6
+MAX_SEQ_LEN = 128
+BERT_NAME = 'bert-base-uncased'
+classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+classes = np.array(classes)
+
+
+
+
+input_ids = layers.Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name='input_ids')
+input_type = layers.Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name='token_type_ids')
+input_mask = layers.Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name='attention_mask')
+inputs = [input_ids, input_type, input_mask]
+bert = TFBertModel.from_pretrained('bert-base-uncased')
+bert_outputs = bert(inputs)
+last_hidden_states = bert_outputs.last_hidden_state
+avg = layers.GlobalAveragePooling1D()(last_hidden_states)
+output = layers.Dense(N_CLASSES, activation="sigmoid")(avg)
+model = keras.Model(inputs=inputs, outputs=output)
+print(f'CurrDir: {os.getcwd()}')
+model.load_weights(os.getcwd() + '/getTox/toxicityModel/toxmodelckpt')
+
+def getToxicity(sentences):
+    enc_sentences = bert_utils.prepare_bert_input(sentences, MAX_SEQ_LEN, 'bert-base-uncased')
+    predictions = model.predict(enc_sentences)
+    
+    sentence_tox_pairs = []
+    for sentence, pred in zip(sentences, predictions):
+        mask = (pred > 0.5).astype(bool)
+        tox = str(classes[mask]) if any(mask) else "not toxic"
+        sentence_tox_pairs.append((sentence.replace("\n", "").strip(), tox, pred[mask]))
+    return sentence_tox_pairs
