c66507613eae2366dfa2e18a5843941be60d9ff0
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2022-01-24 22:17:31 -0500

    New frontend with flask app

---
+++ sabu/sabu.py
@@ -0,0 +1,135 @@
+from flask import Flask, render_template, request
+from twint_wrapper import get_tweets
+
+app = Flask(__name__)
+
+
+#nest_asyncio.apply()
+
+# First time setup
+# 1. pip install flask
+# 2. Windows version:
+# 2. set FLASK_APP=sabu.py
+# 2. Linux version:
+# 2. export FLASK_APP=sabu.py
+
+# 3. git clone --depth=1 https://github.com/twintproject/twint.git
+# 4. cd twint
+# 5. pip3 install . -r requirements.txt
+# 6. pip3 install --user --upgrade -e git+https://github.com/twintproject/twint.git@origin/master#egg=twint
+
+# SET FLASK ENV VARIABLE FOR TEST RUN:
+# $env:FLASK_APP = "market.py"
+# FLASK DEBUG MODE (optional):
+# $env:FLASK_ENV = "development"
+
+# Set default html render page (index.html)
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/index', methods=['POST', 'GET'])
+def search_page():
+
+
+    if request.method == 'POST':
+        #popular_tweets = False
+        # set default parameters (none)
+        popular_tweets = False
+        verified = False
+        username = False
+        filer_retweets = False
+        links = False
+        sabu_ = False
+
+
+        limit = 5  # Default limit of 20
+        if ('popular_tweets' in request.form):
+            popular_tweets = True
+            print('pop')
+        if ('verified' in request.form):
+            verified = True
+            print('veri')
+        if ('username' in request.form):
+            username = True
+            print('user')
+        if ('filter_retweets' in request.form):
+            filter_retweets = True
+            print('filter r')
+        if ('links' in request.form):
+            links = True
+            print('linky')
+        if ('sabu_' in request.form):
+            sabu_ = True
+            print('sabu')
+
+        # gather from until vals
+
+
+        # gather tweet limit val
+        print('Limit entered: ' + str(request.form['limit']))
+
+
+        print(str(request.form['search_input']))
+        data_dict = get_tweets(str(request.form['search_input']), 20, True)
+    else:
+        return render_template('index.html')
+    # Convert dataframe to dictionary since Jinja is not compatible with DF's
+    items = data_dict.tweet.to_dict()
+    #print(items)
+    # tweets = [tweet['tweet'] for tweet in data_dict]
+    # list(items.values())
+    # Test chart.js
+
+    """
+    data = [
+        ("01-01-2020", 1597),
+        ("02-01-2020", 1456),
+        ("03-01-2020", 1908),
+        ("04-01-2020", 896),
+        ("05-01-2020", 755),
+        ("06-01-2020", 453),
+        ("07-01-2020", 1100),
+        ("08-01-2020", 1235),
+        ("09-01-2020", 1478),
+    ]"""
+
+    """
+    sentiment_scores = getSentimentAverage(list(items.values()))
+    search_query = str(request.form['name_input'])
+
+    negativity = "{:.2f}".format(100 * sentiment_scores[0])
+    neutrality = "{:.2f}".format(100 * sentiment_scores[1])
+    positivity = "{:.2f}".format(100 * sentiment_scores[2])
+
+    print("Tweets found using " + search_query + " were " + negativity + "% Negative, " + neutrality + "% Neutral, " + positivity + "% Positive")
+    """
+
+    """
+    # get Toxcity test
+    sentences = np.asarray(list(items.values()))
+
+
+    sentence_tox_pairs = getToxicity(sentences)
+
+    toxic_num = 0
+
+    for pair in sentence_tox_pairs:
+        if (pair > 0.5):
+            toxic_num += 1
+    print('TOXIC NUM: ' + str(toxic_num))
+    print('TOXIC COUNT: ' + str(len(sentence_tox_pairs)))
+    toxic_rating = "{: .2f}".format(float(toxic_num / len(sentence_tox_pairs)))
+    nontoxic = "{: .2f}".format(float(1 - (toxic_num / len(sentence_tox_pairs))))
+    """
+
+    # actual_tweets = items.values()
+    # print(actual_tweets)
+    # items_np = np.fromiter(items.values(),dtype='S128')
+    # print(items_np)
+
+    return render_template('index.html', items=items)
+
+
+
+
+
+# TODO
+#def about_page():
